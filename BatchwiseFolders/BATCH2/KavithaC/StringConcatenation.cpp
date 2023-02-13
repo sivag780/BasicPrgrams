746f0307d268efd7d5c3e4965c067357abe12156
KavithaC-git <kavithachinny415@gmail.com>
2023-02-13 19:21:08 +0530

    Added

---
+++ BatchwiseFolders/BATCH2/KavithaC/StringConcatenation.cpp
@@ -0,0 +1,27 @@
+/*#include<iostream>
+using namespace std;
+void strcat_ur(char *, char *);
+int main()
+{
+    char a[10]="Hi";
+    char b[]="GM";
+    strcat_ur(a,b);
+    cout<<"String a: "<<a<<endl;
+
+    return 0;
+}
+void strcat_ur(char *ptr, char *dptr)
+{
+    while(*ptr!='\0')
+    {
+        ptr++;
+    }
+    while(*dptr!='\0')
+    {
+        *ptr=*dptr;
+        ptr++;
+        dptr++;
+    }
+    *ptr++='\0';
+}
+*/
