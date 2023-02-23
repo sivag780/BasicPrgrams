8ecd4440daed412d112a02b6e16d41e5eedcbd00
srinivasa s <srinivasa.briskybold@gmail.com>
2023-02-23 23:04:50 +0530

    a

--- BatchwiseFolders/sri/virtual functions.cpp
+++ BatchwiseFolders/sri/virtual functions.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 
 using namespace std;
 
@@ -65,4 +65,48 @@ int main()
 	SHAPE=&R;
 	SHAPE->area();
 	return 0;
+}*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class candidate
+{
+	public:
+	char name[30];
+	long long int mobile;
+	float salary;
+
+	candidate()
+	{
+		char a[10]="no name";
+		strcpy(name,a);
+		mobile=1111111111;
+		salary=0.0;
+	}
+
+	void insert(char a[],long long int b,float c)
+	{
+		strcpy(name,a);
+		mobile=b;
+		salary=c;
+	}
+
+	void show()
+	{
+		cout<<"NAME : "<<name<<endl;
+		cout<<"MOBILE : "<<mobile<<endl;
+		cout<<"SALARY : "<<salary<<endl;
+
+	}
+};
+
+void main()
+{
+	candidate A;
+	A.insert("srinivasa",7892848719,80000);
+	A.show();
+	cout<<sizeof(A.name)<<endl;
 }
